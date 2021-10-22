b24ad7357ee7bddd0f732ff2e2521064af9219e7
unknown <you@example.com>
2021-10-21 23:12:59 -0400

    got rid of error message for NavLink

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ import IconButton from "@mui/material/IconButton";
 import AccountCircle from "@mui/icons-material/AccountCircle";
 import MenuItem from "@mui/material/MenuItem";
 import Menu from "@mui/material/Menu";
-import {NavLink} from 'react-router-dom'
+
 
 
 
